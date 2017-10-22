70f22444aa2ac71e01a6a05e334f6081539ca712
aizenbit <aizenbit@gmail.com>
2017-10-22 21:54:32 +0300

    implemented bullets spawn

---
+++ Assets/Scripts/Services/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnManager : NetworkBehaviour
+{
+    /// <summary>
+    /// Спаун пули из оружия типа Firearm
+    /// </summary>
+    [Command]
+    public void CmdSpawnBullet(GameObject caller)
+    {
+        var weapon = caller.GetComponent<Firearm>();
+        GameObject bullet = Instantiate(weapon.bulletPrefab, weapon.bulletSpawnPoint.position, weapon.bulletSpawnPoint.rotation);
+        bullet.GetComponent<BaseBullet>().Team = weapon.Team;
+        bullet.GetComponent<BaseBullet>().Holder = weapon.Holder;
+        bullet.GetComponent<Rigidbody>().velocity += weapon.GetComponent<Rigidbody>().velocity;
+
+        foreach (Collider col in weapon.GetAllColliders())
+        {
+            Physics.IgnoreCollision(col, bullet.GetComponent<Collider>());
+        }
+
+        NetworkServer.Spawn(bullet);
+        RpcSpawnBullet(caller, bullet);
+    }
+
+    /// <summary>
+    /// Обновляет скорость и направление пули на клиенте
+    /// </summary>
+    /// <param name="caller"></param>
+    /// <param name="bullet"></param>
+    [ClientRpc]
+    public void RpcSpawnBullet(GameObject caller, GameObject bullet)
+    {
+        var weapon = caller.GetComponent<Firearm>();
+        bullet.transform.position = weapon.bulletSpawnPoint.position;
+        bullet.transform.rotation = weapon.bulletSpawnPoint.rotation;
+        bullet.GetComponent<Rigidbody>().velocity = weapon.GetComponent<Rigidbody>().velocity;
+    }
+}
--- Assets/Scripts/Weapons/Firearm.cs
+++ Assets/Scripts/Weapons/Firearm.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 
 public class Firearm : BaseWeapon
 {
-    [SerializeField] Transform bulletSpawnPoint;
-    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] public Transform bulletSpawnPoint;
+    [SerializeField] public GameObject bulletPrefab;
     [SerializeField] ParticleSystem muzzleFlashParticle;
     [SerializeField] Text ammoCounter;
 
@@ -19,30 +19,17 @@ public class Firearm : BaseWeapon
 
     protected override void OnPrimaryFire()
     {
-        CmdSpawnBullet();
+        var spawnManager = transform.root.GetComponentInChildren<SpawnManager>();
+        spawnManager.CmdSpawnBullet(this.gameObject);
         muzzleFlashParticle.Play();
         if (ammoCounter != null)
             ammoCounter.text = string.Format("{0}", ammo);
     }
 
-    private Collider[] GetAllColliders()
+    public Collider[] GetAllColliders()
     {
         return transform.root.GetComponentsInChildren<Collider>();
     }
 
-    [Command]
-    void CmdSpawnBullet()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        bullet.GetComponent<BaseBullet>().Team = Team;
-        bullet.GetComponent<BaseBullet>().Holder = Holder;
-        bullet.GetComponent<Rigidbody>().velocity += this.GetComponent<Rigidbody>().velocity;
 
-        foreach (Collider col in GetAllColliders())
-        {
-            Physics.IgnoreCollision(col, bullet.GetComponent<Collider>());
-        }
-        
-        NetworkServer.Spawn(bullet);
-    }
 }
